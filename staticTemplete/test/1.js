460380789c04fef44ecc9182a2a72ce63f5a221d
ybbwfn <ybbwfnta@126.com>
2018-03-20 21:27:11 +0800

    55555

--- staticTemplete/test/1.js
+++ staticTemplete/test/1.js
@@ -1,9 +1,13 @@
-var arr=[];
-arr.push({
+var arr1=[];
+arr1.push({
     name:'张三',
     age:'3',
 })
-arr.push({
+arr1.push({
+    name:'wangwu',
+    age:'5',
+})
+arr1.push({
     name:'李四',
     age:'4',
 })
